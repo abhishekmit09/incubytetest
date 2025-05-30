8d50d28a144c6221be573bca949e1ee8677e52a4
Abhishek <abhishek.mit09@gmail.com>
2025-05-30 15:18:59 +0530

    Green: support newline delimiters along with commas

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -17,9 +17,17 @@
 // }
 
 
+// export function add(numbers: string): number {
+//   if (numbers === "") return 0;
+
+//   const numberList = numbers.split(",").map(num => parseInt(num, 10));
+//   return numberList.reduce((sum, num) => sum + num, 0);
+// }
+
 export function add(numbers: string): number {
   if (numbers === "") return 0;
 
-  const numberList = numbers.split(",").map(num => parseInt(num, 10));
+  const normalized = numbers.replace(/\n/g, ",");
+  const numberList = normalized.split(",").map(num => parseInt(num, 10));
   return numberList.reduce((sum, num) => sum + num, 0);
 }
