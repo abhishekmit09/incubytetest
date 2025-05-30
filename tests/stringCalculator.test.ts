2475d45704c1782ab67dd61f5125ea8cbb80bbb7
Abhishek <abhishek.mit09@gmail.com>
2025-05-30 15:10:17 +0530

    Red: added test for single number input

--- tests/stringCalculator.test.ts
+++ tests/stringCalculator.test.ts
@@ -1,5 +1,9 @@
 import { add } from '../src/stringCalculator';
 
-test('should return 0 for empty string', () => {
-  expect(add("")).toBe(0);
+// test('should return 0 for empty string', () => {
+//   expect(add("")).toBe(0);
+// });
+
+test('should return the number itself for a single number input', () => {
+  expect(add("1")).toBe(1);
 });
