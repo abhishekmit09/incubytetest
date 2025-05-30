9b0de905a9145e4200b7fd76f3c543d8a9ab16cb
Abhishek <abhishek.mit09@gmail.com>
2025-05-30 15:23:54 +0530

    Green: support custom delimiter

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -24,10 +24,27 @@
 //   return numberList.reduce((sum, num) => sum + num, 0);
 // }
 
+// export function add(numbers: string): number {
+//   if (numbers === "") return 0;
+
+//   const normalized = numbers.replace(/\n/g, ",");
+//   const numberList = normalized.split(",").map(num => parseInt(num, 10));
+//   return numberList.reduce((sum, num) => sum + num, 0);
+// }
+
 export function add(numbers: string): number {
   if (numbers === "") return 0;
 
-  const normalized = numbers.replace(/\n/g, ",");
-  const numberList = normalized.split(",").map(num => parseInt(num, 10));
+  let delimiter = /,|\n/; // default delimiters
+  let numberPart = numbers;
+
+  if (numbers.startsWith("//")) {
+    const parts = numbers.split("\n");
+    delimiter = new RegExp(parts[0].slice(2)); // custom delimiter
+    numberPart = parts[1];
+  }
+
+  const numberList = numberPart.split(delimiter).map(num => parseInt(num, 10));
   return numberList.reduce((sum, num) => sum + num, 0);
 }
+
